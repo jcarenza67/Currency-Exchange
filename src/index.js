949e22d68a2dafc521346869919982ab7ac66688
Joseph Wilfong <josephwilfong91@gmail.com>
2023-04-21 09:31:05 -0700

    WIP fetch exchange rate data from API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Currency from '.js/currency.js';
+
+function getExchangeRate(response) {
+  if (response.result === "success") {
+    return response.conversion_rates;
+  } else {
+    throw Error(response.result);
+  }
+}
---
+++ src/js/currency.js
@@ -0,0 +1,20 @@
+export default class Currency {
+  static getExchangeRate() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      request.addEventListener("load", function() {
+        if (this.status === 200) {
+          const exchange = JSON.parse(this.responseText);
+          const rates = getExchangeRate(exchange);
+          resolve(rates);
+        } else {
+          reject(new Error(`Request failed with status ${this.status}`));
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
+            
+    });
+  }
+}
